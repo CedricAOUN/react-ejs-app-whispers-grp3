f566168c83d2a19686f0c03bbe7a097c699b0a10
CedricA <cedric.j.aoun@gmail.com>
2025-06-20 16:30:09 +0200

    trigger deploy attempt

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import { app } from "./server.js";
 const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
 app.listen(port, () => {
   console.log(`Running in http://localhost:${port}`);
 });
